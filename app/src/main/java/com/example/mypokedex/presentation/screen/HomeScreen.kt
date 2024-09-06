0af3b1ee04b4c9dcd72782d493a6270fe43e6a4d
aman <amanavelon@gmail.com>
2024-09-06 17:33:12 +0530

    data class

--- app/src/main/java/com/example/mypokedex/presentation/screen/HomeScreen.kt
+++ app/src/main/java/com/example/mypokedex/presentation/screen/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.example.mypokedex.presentation.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -11,39 +10,28 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.paging.compose.collectAsLazyPagingItems
-import coil.ImageLoader
-import coil.request.CachePolicy
 import com.example.mypokedex.presentation.common.PokemonCard
 import com.example.mypokedex.presentation.viewModels.HomeViewModel
-import org.koin.androidx.compose.getViewModel
+import kotlinx.serialization.Serializable
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(viewModel: HomeViewModel = getViewModel()) {
+fun HomeScreen(navController : NavController, viewModel: HomeViewModel) {
 
     val pokemonList = viewModel.pokemonList.collectAsLazyPagingItems()
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
-    val context = LocalContext.current
-    val imageLoader = remember {
-        ImageLoader.Builder(context)
-            .memoryCachePolicy(CachePolicy.ENABLED)
-            .diskCachePolicy(CachePolicy.ENABLED)
-            .crossfade(true)
-            .build()
-    }
 
     Scaffold(
         modifier = Modifier
-            .nestedScroll(scrollBehavior.nestedScrollConnection)
-            .background(MaterialTheme.colorScheme.background),
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        containerColor= MaterialTheme.colorScheme.background,
+
         topBar = {
             LargeTopAppBar(
                 title = {
@@ -51,27 +39,37 @@ fun HomeScreen(viewModel: HomeViewModel = getViewModel()) {
                         "Pokedex",
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis,
-                        fontWeight = FontWeight.Bold
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.surfaceBright
                     )
                 },
                 navigationIcon = {},
                 scrollBehavior = scrollBehavior,
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor =  MaterialTheme.colorScheme.background,
+                            scrolledContainerColor = MaterialTheme.colorScheme.background
+                )
             )
         },
         content = { innerPadding ->
             LazyVerticalGrid(
                 columns = GridCells.Fixed(2),
                 contentPadding = innerPadding,
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                verticalArrangement = Arrangement.SpaceAround,
+                horizontalArrangement = Arrangement.SpaceEvenly
             ) {
                 items(pokemonList.itemCount) { pokemonIndex ->
                     pokemonList[pokemonIndex]?.let { pokemon ->
                         PokemonCard(
-                            name = pokemon.name,
+                            pokemonName = pokemon.name,
                             image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonIndex + 1}.png",
-                            imageLoader = imageLoader,
-                            context = context
+                            onClick = {
+                               navController.navigate(PokemonDetailScreen(
+                                   pokemonName = pokemon.name,
+                                   image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonIndex + 1}.png"
+                               ))
+
+                            }
                         )
                     }
                 }
@@ -81,3 +79,6 @@ fun HomeScreen(viewModel: HomeViewModel = getViewModel()) {
 }
 
 
+
+@Serializable
+object HomeScreen
