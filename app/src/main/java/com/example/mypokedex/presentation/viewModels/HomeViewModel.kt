9930ca9d9b4c9adafaf3a4baa4557d9dd416387d
aman <amanavelon@gmail.com>
2024-09-06 17:33:26 +0530

    home viewModel

--- app/src/main/java/com/example/mypokedex/presentation/viewModels/HomeViewModel.kt
+++ app/src/main/java/com/example/mypokedex/presentation/viewModels/HomeViewModel.kt
@@ -1,24 +1,33 @@
 package com.example.mypokedex.presentation.viewModels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.example.mypokedex.data.model.response.PokemonDetails
 import com.example.mypokedex.data.model.response.ResultPokemon
+import com.example.mypokedex.domain.repo.PokemonRepository
+import com.example.mypokedex.util.ApiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class HomeViewModel(
-    private val pokemonPagingSource: PokemonPagingSource
+    private val pokemonPagingSource: PokemonPagingSource,
+    private val pokemonRepository: PokemonRepository
 
 ):ViewModel() {
 
     private val _pokemonList: MutableStateFlow<PagingData<ResultPokemon>> =
         MutableStateFlow(PagingData.empty())
     var pokemonList = _pokemonList.asStateFlow()
+        private set
+    private val _pokemonData: MutableStateFlow<PokemonDetails?> = MutableStateFlow(null)
+    var pokemonData = _pokemonData.asStateFlow()
         private set
 
     init {
@@ -32,6 +41,14 @@ class HomeViewModel(
             }.flow.cachedIn(viewModelScope).collect {
                 _pokemonList.value = it
             }
+        }
+
+    }
+
+    fun getPokemonData(pokemonName:String) {
+        viewModelScope.launch {
+            val data = pokemonRepository.getPokemonDetails(pokemonName )
+            _pokemonData.value = data
         }
     }
 
