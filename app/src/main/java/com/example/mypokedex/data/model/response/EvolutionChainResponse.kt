c6e6569cb08c8926923fd7a30eea655db1ad3871
aman <amanavelon@gmail.com>
2024-09-13 18:32:45 +0530

    response for evolution added

--- app/src/main/java/com/example/mypokedex/data/model/response/EvolutionChainResponse.kt
+++ app/src/main/java/com/example/mypokedex/data/model/response/EvolutionChainResponse.kt
@@ -2,12 +2,48 @@ package com.example.mypokedex.data.model.response
 
 
 data class EvolutionChainResponse(
-    val chain: EvolutionChainNode
+    val id: Int,
+    val baby_trigger_item: Any?,
+    val chain: Chain
 )
 
-data class EvolutionChainNode(
-    val species: Species,
-    val evolves_to: List<EvolutionChainNode>
+data class Chain(
+    val evolution_details: List<EvolutionDetail>,
+    val evolves_to: List<EvolveTo>,
+    val species: Species
 )
+
+data class EvolveTo(
+    val evolution_details: List<EvolutionDetail>,
+    val evolves_to: List<EvolveTo>,
+    val species: Species
+)
+data class EvolutionDetail(
+    val gender: Any?,
+    val held_item: Any?,
+    val item: Any?,
+    val known_move: Any?,
+    val known_move_type: Any?,
+    val location: Any?,
+    val min_affection: Any?,
+    val min_beauty: Any?,
+    val min_happiness: Any?,
+    val min_level: Int?,
+    val needs_overworld_rain: Boolean,
+    val party_species: Any?,
+    val party_type: Any?,
+    val relative_physical_stats: Any?,
+    val time_of_day: String,
+    val trade_species: Any?,
+    val trigger: Trigger,
+    val turn_upside_down: Boolean
+)
+
+data class Trigger(
+    val name: String,
+    val url: String
+)
+
+
 
 
