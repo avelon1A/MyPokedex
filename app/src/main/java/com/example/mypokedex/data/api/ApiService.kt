0019633209cb38968d4f9f8745ecdd8efc8407b2
aman <amanavelon@gmail.com>
2024-09-13 18:30:32 +0530

    call adapter added

--- app/src/main/java/com/example/mypokedex/data/api/ApiService.kt
+++ app/src/main/java/com/example/mypokedex/data/api/ApiService.kt
@@ -7,6 +7,7 @@ import com.example.mypokedex.data.model.response.PokemonSpeciesResponse
 import com.example.mypokedex.data.model.response.PokemonSpeciesResponseText
 import com.example.mypokedex.data.model.response.TypeResponse
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -21,7 +22,7 @@ interface ApiService {
 
     @GET("pokemon/{name}")
     suspend fun getPokemonData(
-        @Path("name") name: String
+        @Path("name") name: Any
 
     ):PokemonDetails
 
@@ -42,8 +43,9 @@ interface ApiService {
     ): PokemonSpeciesResponseText
 
     @GET("evolution-chain/{id}")
-    fun getEvolutionChain(
-        @Path("id") id: Int): EvolutionChainResponse
+   suspend fun getEvolutionChain(
+        @Path("id") id: Int): Response<EvolutionChainResponse>
+
 
 
 }
